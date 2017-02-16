5ea20c94db7ad2f72f0a04cbb180d9537399cb21
Javier <jvrcueto@outlook.com>
2017-02-16 17:18:16 -0500

    test button

--- app.js
+++ app.js
@@ -2,8 +2,23 @@
 let GAME = {};
 Game();
 
+let counter = 0;
+
 let testBtn = $('#test-btn');
 testBtn.click(() => {
+  counter++;
+  if (counter === 1) {
+    GAME.moveArmies(0,1);
+  }
+  else if (counter === 2) {
+    GAME.moveArmies(3,2);
+  }
+  else if (counter === 3) {
+    GAME.moveArmies(1,2);
+  }
+  else if (counter === 4) {
+    GAME.fight();
+  }
 });
 
 
